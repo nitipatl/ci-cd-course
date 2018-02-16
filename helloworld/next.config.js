667306cfffaa673f3a7d01a3b84555394c4a949b
Nitipat L <iamsvz@gmail.com>
2018-02-16 16:25:28 +0700

    add static export command

---
+++ helloworld/next.config.js
@@ -0,0 +1,9 @@
+module.exports = {
+  exportPathMap: function() {
+    return {
+      '/': { page: '/' },
+      '/hello': { page: '/hello' },
+      '/hello/taey': { page: '/hello' },
+    }
+  }
+}
