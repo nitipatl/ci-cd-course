4ff76578f03b8fb38956e62aca2c66b08f081782
Nitipat L <iamsvz@gmail.com>
2018-02-16 17:48:47 +0700

    add dup to code

--- helloworld/pages/hello.js
+++ helloworld/pages/hello.js
@@ -1,5 +1,6 @@
 import React from 'react'
 import _greeting from '../libs/greeting'
+import _greeting2 from '../libs/greeting.dup'
 export default ({ url: { query: { name } } }) => (
   <p>{ _greeting(name) }</p>
 )
