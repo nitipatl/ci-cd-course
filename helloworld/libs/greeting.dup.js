11bed80ee4179e1ea08d83f8d15a3e107a91b6d8
Nitipat L <iamsvz@gmail.com>
2018-02-16 17:51:52 +0700

    add more dups

--- helloworld/libs/greeting.dup.js
+++ helloworld/libs/greeting.dup.js
@@ -1,1 +1,4 @@
+const gr1 = (name) => (name ? `Hi, ${name}` : `Hello, Anonymous`)
+const gr2 = (name) => (name ? `Hi, ${name}` : `Hello, Anonymous`)
+const gr3 = (name) => (name ? `Hi, ${name}` : `Hello, Anonymous`)
 export default (name) => (name ? `Hi, ${name}` : `Hello, Anonymous`)
